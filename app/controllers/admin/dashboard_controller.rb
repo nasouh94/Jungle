f47bdb64f7f47e94d574b5c8ce5f1f14262019c5
nasouh94 <nasouh@live.com>
2019-11-13 17:45:07 +0000

    admin dashboard page now displays stats about inventory

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products_count= Product.count
+    @products_sum = Product.sum(:quantity)
+    @categories = Category.count
   end
+
 end
